8105330d08098360596f69b411646f2034de57a3
feng19 <feng_19@foxmail.com>
2020-12-04 12:02:58 +0800

    feat: format code

--- lib/ex_aliyun_sls/client.ex
+++ lib/ex_aliyun_sls/client.ex
@@ -5,6 +5,10 @@ defmodule ExAliyunSls.Client do
   use Tesla
   alias ExAliyunSls.LogGroup
 
+  @version "0.6.0"
+  @sign_method "hmac-sha1"
+  @content_type "application/x-protobuf"
+
   adapter({Tesla.Adapter.Finch, [name: ExAliyunSls.Finch]})
   plug(Tesla.Middleware.Timeout, timeout: 5_000)
   plug(Tesla.Middleware.Retry, delay: 5_000, max_retries: 5)
@@ -20,16 +24,14 @@ defmodule ExAliyunSls.Client do
     date = Timex.now() |> Timex.format!("%a, %d %b %Y %H:%M:%S GMT", :strftime)
     body_length = body |> byte_size |> to_string
     md5 = :crypto.hash(:md5, body) |> Base.encode16(case: :upper)
-    version = "0.6.0"
-    sign_method = "hmac-sha1"
 
     canonicalized_log_headers =
-      "x-log-apiversion:#{version}\nx-log-bodyrawsize:#{body_length}\nx-log-signaturemethod:#{
-        sign_method
+      "x-log-apiversion:#{@version}\nx-log-bodyrawsize:#{body_length}\nx-log-signaturemethod:#{
+        @sign_method
       }"
 
     content =
-      "POST\n#{md5}\napplication/x-protobuf\n#{date}\n#{canonicalized_log_headers}\n#{profile.resource}"
+      "POST\n#{md5}\n#{@content_type}\n#{date}\n#{canonicalized_log_headers}\n#{profile.resource}"
 
     signature = :crypto.hmac(:sha, profile.access_key, content) |> Base.encode64()
     authorization = "LOG " <> profile.access_key_id <> ":" <> signature
@@ -37,10 +39,10 @@ defmodule ExAliyunSls.Client do
     headers = [
       {"Content-Length", body_length},
       {"Content-MD5", md5},
-      {"content-type", "application/x-protobuf"},
+      {"content-type", @content_type},
       {"x-log-bodyrawsize", body_length},
-      {"x-log-apiversion", version},
-      {"x-log-signaturemethod", sign_method},
+      {"x-log-apiversion", @version},
+      {"x-log-signaturemethod", @sign_method},
       {"Host", host},
       {"Date", date},
       {"Authorization", authorization}
