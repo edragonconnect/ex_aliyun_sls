68223cb3e1f793c6fd5a69884ad87ebdeed9e1db
feng19 <feng_19@foxmail.com>
2020-12-10 11:17:28 +0800

    feat: upgrade elixir minimum version to 1.7

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule ExAliyunSls.MixProject do
     [
       app: :ex_aliyun_sls,
       version: "0.2.0",
-      elixir: "~> 1.6",
+      elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       docs: [extras: ["README.md"]],
