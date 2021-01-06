a726fbdad285a11fb769d395a4138e5c9cd9c9e4
feng19 <feng_19@foxmail.com>
2021-01-06 10:30:14 +0800

    update version to 0.3.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExAliyunSls.MixProject do
   def project do
     [
       app: :ex_aliyun_sls,
-      version: "0.3.0",
+      version: "0.3.1",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
