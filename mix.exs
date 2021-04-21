e0abd34600744067c8b3a5168a5e8aac4707be60
feng19 <feng_19@foxmail.com>
2021-04-21 23:27:20 +0800

    dump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExAliyunSls.MixProject do
   def project do
     [
       app: :ex_aliyun_sls,
-      version: "0.3.2",
+      version: "0.3.3",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
